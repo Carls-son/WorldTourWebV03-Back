eaf6f3a7a9b31539defebe3a39240bbd2920647d
TwiceBaked <gabrieljcarlson@lewisu.edu>
2025-08-28 14:53:56 -0500

    added cors, connected to front end

--- StatsApi/Program.cs
+++ StatsApi/Program.cs
@@ -5,12 +5,27 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services
 builder.Services.AddControllers();
+
+// Add CORS policy
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:5173") // your React dev server
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
+});
+
 builder.Services.AddDbContext<StatsDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
+
+app.UseCors("AllowReactApp");
 app.UseAuthorization();
 
 app.MapControllers();
